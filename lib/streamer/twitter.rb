45c7f18cf491fdc4c4427fea2838872597248053
Ryo NAKAMURA <r7kamura@gmail.com>
2011-04-02 14:16:41 +0900

    add some command about twitter

--- lib/streamer/twitter.rb
+++ lib/streamer/twitter.rb
@@ -69,8 +69,8 @@ module Streamer
   end
 
   init do
-    config[:consumer_key]     ||= 'RmzuwQ5g0SYObMfebIKJag'
-    config[:consumer_secret]  ||= 'V98dYYmWm9JoG7qfOF0jhJaVEVW3QhGYcDJ9JQSXU'
+    config[:consumer_key]     ||= 'grQ0RQoR0hjUtLrVPdHdw'
+    config[:consumer_secret]  ||= 'qAw9xeOlBEfPdjYEc278otBMCwPLW0bbPxUDChI'
     @twitter = TwitterOAuth::Client.new(config.slice(:consumer_key, :consumer_secret, :token, :secret))
     get_access_token unless self.config[:token] && self.config[:secret]
 
@@ -121,8 +121,102 @@ module Streamer
       puts result.join(" ")
     end
 
-    command :recent do
+    command :recent, :help => "show recent tweets" do
       puts_items twitter.home_timeline
+    end
+
+    command :tw, :help => "tweet (ex => 'tw hello world now!!')" do |m|
+      async { twitter.update(m[1]) } if confirm("update '#{m[1]}'")
+    end
+
+    command :mentions, :help => "show tweets menioned(replyed) to me" do
+      puts_items twitter.mentions
+    end
+
+    command :follow, :help => "follow specified twitter user" do |m|
+      async { twitter.friend(m[1]) }
+    end
+
+    command :unfollow, :help => "unfollow specified twitter user" do |m|
+      async { twitter.unfriend(m[1]) }
+    end
+
+    command :user do |m|
+      ap twitter.show(m[1]).slice(*%w(id screen_name name profile_image_url description url location time_zone lang protected))
+    end
+
+    command :search do |m|
+      puts_items twitter.search(m[1])["results"].each { |s|
+        s["user"] = {"screen_name" => s["from_user"]}
+        s["_disable_cache"] = true
+        words = m[1].split(/\s+/).reject{|x| x[0] =~ /^-|^(OR|AND)$/ }.map{|x|
+          case x
+          when /^from:(.+)/, /^to:(.+)/
+            $1
+          else
+            x
+          end
+        }
+        s["_highlights"] = words
+      }
+    end
+
+    command %r|^:retweet\s+(\d+)$|, :as => :retweet do |m|
+      target = twitter.status(m[1])
+      if confirm("retweet 'RT @#{target["user"]["screen_name"]}: #{target["text"].e}'")
+        async { twitter.retweet(m[1]) }
+      end
+    end
+
+    command %r|^:retweet\s+(\d+)\s+(.*)$|, :as => :retweet do |m|
+      target = twitter.status(m[1])
+      text = "#{m[2]} RT @#{target["user"]["screen_name"]}: #{target["text"].e} (#{target["id"]})"
+      if confirm("unofficial retweet '#{text}'")
+        async { twitter.update(text) }
+      end
+    end
+
+    command :favorite do |m|
+      async { twitter.favorite(m[1]) }
+    end
+
+    command :unfavorite do |m|
+      async { twitter.unfavorite(m[1]) }
+    end
+
+    command :retweeted_by_me do
+      puts_items twitter.retweeted_by_me
+    end
+
+    command :retweeted_to_me do
+      puts_items twitter.retweeted_to_me
+    end
+
+    command :retweets_of_me do
+      puts_items twitter.retweets_of_me
+    end
+
+    command :block do |m|
+      async { twitter.block(m[1]) }
+    end
+
+    command :unblock do |m|
+      async { twitter.unblock(m[1]) }
+    end
+
+    command :report_spam do |m|
+      async { twitter.report_spam(m[1]) }
+    end
+
+    command :messages do
+      puts_items twitter.messages.each { |s|
+        s["user"] = {"screen_name" => s["sender_screen_name"]}
+        s["_disable_cache"] = true
+      }
+    end
+
+    command %r|^:message (\w+)\s+(.*)|, :as => :message do |m|
+      async { twitter.message(*m[1, 2]) } if confirm("message '#{m[2]}' to @#{m[1]}")
     end
 
     connect_twitter
