e0be0732376f61b495d4dfd2e6f429af7dab76cf
Ryo NAKAMURA <r7kamura@gmail.com>
2011-04-03 19:04:06 +0900

    use Hash in gsub String#u

--- lib/streamer/ext.rb
+++ lib/streamer/ext.rb
@@ -1,19 +1,15 @@
-# -*- coding: utf-8 -*-
-
 class String
   def c(*codes)
-    "\e[#{codes.join;}m#{self}\e[0m"
+    "\e[#{codes.join(";")}m#{self}\e[0m"
   end
 
   def u
-    gsub(/&(lt|gt|amp|quot|apos);/) do |s|
-      {
-        'amp'   => '&',
-        'lt'    => '<',
-        'gt'    => '>',
-        'apos'  => "'",
-        'quot'  => '"',
-      }[$1]
-    end
+    gsub(/&(?:lt|gt|amp|quot|apos);/, {
+      'amp'   => '&',
+      'lt'    => '<',
+      'gt'    => '>',
+      'apos'  => "'",
+      'quot'  => '"',
+    })
   end
 end
